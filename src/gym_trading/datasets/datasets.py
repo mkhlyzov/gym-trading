482947c909d876b766dc2f9fe353bada94381fab
mkhlyzov <s-man911@ukr.net>
2023-02-19 11:07:21 +0300

    Fixed data downloading headers

--- src/gym_trading/datasets/datasets.py
+++ src/gym_trading/datasets/datasets.py
@@ -10,26 +10,31 @@ ENDPOINTS = {
 def load_asset_from_disc(asset_name: str) -> pandas.DataFrame:
     """
     > Loads a CSV file from the `data` directory and returns a `pandas.DataFrame` object
-    
+
     :param asset_name: The name of the asset you want to load
     :type asset_name: str
     :return: A dataframe
     """
-    asset_path = pathlib.Path(__file__).resolve().parent / 'data' / "".join([asset_name, ".csv"])
+    asset_path = (
+        pathlib.Path(__file__).resolve().parent / "data" / "".join([asset_name, ".csv"])
+    )
     asset_df = pandas.read_csv(asset_path)
+    asset_df['date'] = pandas.to_datetime(asset_df['date'])
     return asset_df
 
 
 def load_asset_from_internet(asset_name: str) -> pandas.DataFrame:
     """
     > This function takes in a string, and returns a pandas dataframe
-    
+
     :param asset_name: The name of the asset you want to load
     :type asset_name: str
     :return: A dataframe with the asset data.
     """
-    path = ENDPOINTS.get(asset_name, None)
-    asset_df = pandas.read_csv(path, header=1)
+    # https://stackoverflow.com/questions/62278538/pd-read-csv-produces-httperror-http-error-403-forbidden
+    url = ENDPOINTS.get(asset_name, None)
+    storage_options = {"User-Agent": "Mozilla/5.0"}
+    asset_df = pandas.read_csv(url, header=1, storage_options=storage_options)
     asset_df.sort_values(by=["date"], inplace=True, ignore_index=True)
     return asset_df
 
@@ -37,10 +42,12 @@ def load_asset_from_internet(asset_name: str) -> pandas.DataFrame:
 def fetch_asset(asset_name: str) -> None:
     """
     > It downloads a CSV file from the internet, and saves it to a local file
-    
+
     :param asset_name: The name of the asset you want to fetch
     :type asset_name: str
     """
     asset_df = load_asset_from_internet(asset_name=asset_name)
-    to_path = pathlib.Path(__file__).resolve().parent / 'data' / "".join([asset_name, ".csv"])
+    to_path = (
+        pathlib.Path(__file__).resolve().parent / "data" / "".join([asset_name, ".csv"])
+    )
     asset_df.to_csv(to_path, index=False)
