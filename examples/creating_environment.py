d81261d937cba37450b1c047f262ddd010ceb9e2
mkhlyzov <s-man911@ukr.net>
2023-03-05 15:44:22 +0300

    examples upd

--- examples/creating_environment.py
+++ examples/creating_environment.py
@@ -8,11 +8,11 @@ env = gym_trading.envs.TradingEnv(df=gym_trading.datasets.BITCOIN_USD_1H)
 # env_fn = lambda: gym_trading.envs.TradingEnv(df=df=gym_trading.datasets.BITCOIN_USD_1H)
 # env = gym.vector.SyncVectorEnv([env_fn for _ in range(16)])
 #%%
-obs = env.reset()
+obs, info = env.reset()
 num_steps = 0
 while True:
     action = env.action_space.sample()
-    new_obs, reward, terminated, truncated, info = env.step(action)
+    obs, reward, terminated, truncated, info = env.step(action)
     num_steps += 1
     if terminated or truncated:
         break
