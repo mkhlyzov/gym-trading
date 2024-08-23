15e17038f5ac80e50717b7adf0551aa89b37f673
mkhlyzov <s-man911@ukr.net>
2024-08-23 14:03:39 +0300

    MixedEnv changed on-reset sampling to weight-based.

--- src/gym_trading/envs/mixed_env.py
+++ src/gym_trading/envs/mixed_env.py
@@ -8,14 +8,15 @@ from gym_trading.envs.base_env import BaseTradingEnv
 
 class MixedEnv(gymnasium.Env):
     """
-    Wrapper that manages multiple gym_tarding environments.
+    Wrapper-class that manages multiple gym_tarding environments.
     """
     def __init__(self, envs: List[BaseTradingEnv]) -> None:
         self.envs = envs
+        self.weights = [len(env.df) for env in envs]
         self.current_env = self.envs[0]
 
     def reset(self, **kwargs) -> Tuple[Any, Dict]:
-        self.current_env = random.choice(self.envs)
+        self.current_env = random.choices(self.envs, self.weights)
         return self.current_env.reset(**kwargs)
     
     def step(self, action: Any) -> Tuple[Any, SupportsFloat, bool, bool, Dict[str, Any]]:
