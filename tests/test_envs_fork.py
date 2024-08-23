3b96f71e68ab15d52a5e57570d33d433a63c0d8e
mkhlyzov <s-man911@ukr.net>
2024-08-23 09:28:12 +0300

    Added new tests for step reward.

--- tests/test_envs_fork.py
+++ tests/test_envs_fork.py
@@ -16,7 +16,7 @@ def constant_price() -> pandas.DataFrame:
     It creates a DataFrame with 10,000 rows, each row having a constant price
     :return: A dataframe with a constant price.
     """
-    size = 10_000
+    size = 20_000
     df = pandas.DataFrame()
     df[["open", "high", "low", "close"]] = numpy.ones((size, 4), dtype=numpy.float64)
     df["date"] = [
@@ -32,7 +32,7 @@ def linear_price() -> pandas.DataFrame:
     It creates a DataFrame with a linear price series
     :return: A dataframe with a linear price series.
     """
-    size = 10_000
+    size = 20_000
     price_start = 100
     price_end = 200
     df = pandas.DataFrame()
@@ -101,4 +101,36 @@ class TestBaseFunctionality:
         env.step(Position.LONG)
         env.step(Position.SHORT)
         assert env._position == Position.SHORT
+    
+    def test_reward_depends_on_comission(self, instance: BaseTradingEnv) -> None:
+        env = instance(df=gym_trading.datasets.BITCOIN_USD_1H)
+        env.reset()
+        env.price.loc[env.price.index] = 1
+        env._comission_fee = 0.01
+        for i in range(env._idx_last - env._idx_first - 1):
+            pos = env._position.value
+            action = numpy.random.randint(3)
+            _, r, _, _, _ = env.step(action)
+            assert (action == pos) == (r == 0)
+
+    def test_reward_correlates_with_price_movement(self, instance: BaseTradingEnv) -> None:
+        env = instance(df=gym_trading.datasets.BITCOIN_USD_1H)
+
+        # price moves up ==> reward for Long is positive
+        env.reset()
+        env.price.loc[env.price.index] = numpy.arange(100, 100 + len(env.price))
+        env._comission_fee = 0.
+        for i in range(env._idx_last - env._idx_first - 1):
+            action = numpy.random.randint(3)
+            _, r, _, _, _ = env.step(action)
+            assert numpy.sign(r) == numpy.sign(action - 1)
+        
+        # price moves down ==> reward for Short is positive
+        env.reset()
+        env.price.loc[env.price.index] = numpy.arange(100, 100 + len(env.price))[::-1]
+        env._comission_fee = 0.
+        for i in range(env._idx_last - env._idx_first - 1):
+            action = numpy.random.randint(3)
+            _, r, _, _, _ = env.step(action)
+            assert numpy.sign(r) == numpy.sign(1 - action)
     
